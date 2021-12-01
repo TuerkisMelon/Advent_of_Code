4d2b1d0fb6b5977caca4ea61294c319c18509332
TuerkisMelon <95377151+TuerkisMelon@users.noreply.github.com>
2021-12-01 19:54:58 +0100

    Update Day 1.py

--- Year 2021/Day 1.py
+++ Year 2021/Day 1.py
@@ -23,9 +23,9 @@ if __name__ == "__main__":
     same = 0
     old_num = 100000
 
-    for num in vallist:
+    for num in vallist:            
         num = int(num)
-        if old_num < num:
+        if old_num < num:               # check if old number is larger than new number
             larger += 1
         elif old_num > num:
             smaller += 1
@@ -33,7 +33,7 @@ if __name__ == "__main__":
             same += 1
         count += 1
         old_num = num
-    print(f"old_num: {old_num} larger: {larger} smaller: {smaller} same: {same} count: {count}")
+    print(f"PART1= old_num: {old_num} larger: {larger} smaller: {smaller} same: {same} count: {count}")    # printing out the Result
 
     
     count = 0                   # Part 2
@@ -46,10 +46,9 @@ if __name__ == "__main__":
     for num in vallist:
         numb = (0, 0, 0)
         num = int(num)
-        if count + 3 >= len(vallist):
+        if count + 3 >= len(vallist):           # check if count is on the end of the list 
             temp = count - len(vallist)
-            print(temp)
-            if temp == -3:
+            if temp == -3:      
                 numb = (vallist[count], vallist[count + 1], vallist[count + 2])
             elif temp == -2:
                 numb = (vallist[count], vallist[count + 1], 0)
@@ -58,14 +57,12 @@ if __name__ == "__main__":
         else:
             numb = (vallist[count], vallist[count + 1], vallist[count + 2])
 
-        if old_num < int(numb[0]) + int(numb[1]) + int(numb[2]):
-            print(old_num)
+        if old_num < int(numb[0]) + int(numb[1]) + int(numb[2]):            # check if old number is smaller than new number
             larger += 1
         elif old_num == int(numb[0]) + int(numb[1]) + int(numb[2]):
             same += 1
         elif old_num > int(numb[0]) + int(numb[1]) + int(numb[2]):
             smaller += 1
         old_num = int(numb[0]) + int(numb[1]) + int(numb[2])
-        print(old_num)
         count += 1
-    print(f"larger: {larger} smaller: {smaller} same: {same} count: {count}")
+    print(f"PART2= larger: {larger} smaller: {smaller} same: {same} count: {count}")
